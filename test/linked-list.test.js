29a7fd52460675c0d83a89d0f2c511a640b23ff2
ronnie1734 <rlewwwis@yahoo.com>
2018-05-05 16:44:30 -0700

    almost done!

--- test/linked-list.test.js
+++ test/linked-list.test.js
@@ -138,7 +138,7 @@ describe('O(N) Methods (medium difficulty)', function(){
     expect(list.root).toBe(null)
   });
 
-  test.skip('list.remove(value) should remove node from front of list', () => {
+  test('list.remove(value) should remove node from front of list', () => {
     let list = LinkedList.fromArray([0,1,2,3,4,5,6,7,8])
     list.remove(0);
     expect(list.root.value).toBe(1);

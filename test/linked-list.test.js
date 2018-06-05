b7d0bc7860353fe9dd50bc29e2f4435de7ef3a0f
AaronBruce <aaron.bruce@comcast.net>
2018-06-05 09:57:12 -0700

    finished lab

--- test/linked-list.test.js
+++ test/linked-list.test.js
@@ -52,7 +52,7 @@ describe('Essential creation tests', function(){
 });
 
 describe('O(1) Methods (easy difficulty)', function(){
-  test.skip('list.isEmpty() should be true for empty list', () => {
+  test('list.isEmpty() should be true for empty list', () => {
     let list = new LinkedList();
     expect(list.isEmpty()).toEqual(true)
   });
@@ -98,14 +98,14 @@ describe('O(N) Methods (medium difficulty)', function(){
     expect(list.size()).toEqual(4)
   });
 
-  test.skip('list.append(value) should append node to empty list', () => {
+  test('list.append(value) should append node to empty list', () => {
     let list = new LinkedList();
     list.append(0);
     expect(list.root.value).toBe(0);
   });
 
 
-  test.skip('list.append(value) should append node to non-empty list', () => {
+  test('list.append(value) should append node to non-empty list', () => {
     let list = new LinkedList();
     list.append(0);
     list.append(3);
@@ -115,7 +115,7 @@ describe('O(N) Methods (medium difficulty)', function(){
     expect(list.root.next.next.value).toEqual(4);
   });
 
-  test.skip('list.getNth returns the node at a specific index', () => {
+  test('list.getNth returns the node at a specific index', () => {
     let list = LinkedList.fromArray([1,2,3,4]);
     expect(list.getNth(0)).toEqual(list.root);
     expect(list.getNth(1)).toEqual(list.root.next);
@@ -123,33 +123,33 @@ describe('O(N) Methods (medium difficulty)', function(){
     expect(list.getNth(3)).toEqual(list.root.next.next.next);
   });
 
-  test.skip('list.find(value) return fist node containg value', () => {
+  test('list.find(value) return fist node containg value', () => {
     let list = LinkedList.fromArray([1,2,3,2,1]);
     let result = list.find(2);
     expect(list.root.next).toEqual(result);
   });
 
-  test.skip('list.remove(value) should remove node from one-element list', () => {
+  test('list.remove(value) should remove node from one-element list', () => {
     let list = new LinkedList()
     list.append(42);
     list.remove(42);
     expect(list.root).toBe(null)
   });
 
-  test.skip('list.remove(value) should remove node from front of list', () => {
+  test('list.remove(value) should remove node from front of list', () => {
     let list = LinkedList.fromArray([0,1,2,3,4,5,6,7,8])
     list.remove(0);
     expect(list.root.value).toBe(1);
   });
 
-  test.skip('list.remove(value) should remove node from middle of list', () => {
+  test('list.remove(value) should remove node from middle of list', () => {
     let list = LinkedList.fromArray([0,1,2,3,4,5,6,7,8])
     list.remove(3);
     expect(list.root.next.next.value).toBe(2);
     expect(list.root.next.next.next.value).toBe(4);
   });
 
-  test.skip('list.remove(value) should remove node from end of list', () => {
+  test('list.remove(value) should remove node from end of list', () => {
     let list = LinkedList.fromArray([0,1,2,3,4,5,6,7,8])
     list.remove(8);
     expect(list.root.next.next.next.next.next.next.next.value).toBe(7);
@@ -158,7 +158,7 @@ describe('O(N) Methods (medium difficulty)', function(){
 });
 
 describe('Convenience methods', () => {
-  test.skip('list.getFirst() list.getSecond() and list.getThird() (easy difficulty)', () => {
+  test('list.getFirst() list.getSecond() and list.getThird() (easy difficulty)', () => {
     let list = LinkedList.fromArray([1,2,3,4]);
     expect(list.getFirst()).toEqual(list.root);
     expect(list.getSecond()).toEqual(list.root.next);
@@ -169,13 +169,13 @@ describe('Convenience methods', () => {
 });
 
 describe('Stretch Goal: create getNthFromLast convenience methods (hard difficulty)', () => {
-  test.skip('list.getLast() should return last node', () => {
+  test('list.getLast() should return last node', () => {
     let list = LinkedList.fromArray([3, 4, 5, 6]);
     let result = list.getLast();
     expect(result.value).toEqual(6);
   });
 
-  test.skip('list.getNthFromLast() should return nth from last node', () => {
+  test('list.getNthFromLast() should return nth from last node', () => {
     let list = LinkedList.fromArray([3, 4, 5, 6]);
     let result = list.getNthFromLast(0);
     expect(result.value).toEqual(6);
@@ -190,13 +190,13 @@ describe('Stretch Goal: create getNthFromLast convenience methods (hard difficul
     expect(result.value).toEqual(3);
   });
 
-  test.skip('list.getSecondFromLast() should return second to last node', () => {
+  test('list.getSecondFromLast() should return second to last node', () => {
     let list = LinkedList.fromArray([3, 4, 5, 6]);
     let result = list.getSecondFromLast();
     expect(result.value).toEqual(5);
   });
 
-  test.skip('list.getThirdFromLast() should return third from last node', () => {
+  test('list.getThirdFromLast() should return third from last node', () => {
     let list = LinkedList.fromArray([3, 4, 5, 6]);
     let result = list.getThirdFromLast();
     expect(result.value).toEqual(4);
@@ -204,23 +204,23 @@ describe('Stretch Goal: create getNthFromLast convenience methods (hard difficul
 });
 
 describe('Stretch Goal: Common Interview Questions', () => {
-  test.skip('list.hasCycle() returns false when there is no loop in the list', () => {
+  test('list.hasCycle() returns false when there is no loop in the list', () => {
     let list = LinkedList.fromArray([4, 5, 6]);
     expect(list.hasCycle()).toEqual(false);
   });
 
-  test.skip('list.hasCycle() returns false when the list is empty', () => {
+  test('list.hasCycle() returns false when the list is empty', () => {
     let list = LinkedList.fromArray([]);
     expect(list.hasCycle()).toEqual(false);
   });
 
-  test.skip('list.hasCycle() returns true if there\'s just one node with a loop', () => {
+  test('list.hasCycle() returns true if there\'s just one node with a loop', () => {
     let list = LinkedList.fromArray([1]);
     list.root.next = list.root;
     expect(list.hasCycle()).toEqual(true);
   });
 
-  test.skip('list.hasCycle() returns true when there is a loop in the list', () => {
+  test('list.hasCycle() returns true when there is a loop in the list', () => {
     let list1 = LinkedList.fromArray([4, 5, 6]);
     let list2 = LinkedList.fromArray([4, 5, 6]);
     let list3 = LinkedList.fromArray([4, 5, 6]);
@@ -241,7 +241,7 @@ describe('Stretch Goal: Common Interview Questions', () => {
     expect(list4.hasCycle()).toEqual(true);
   });
 
-  test.skip('list.getMiddle() should return middle node', () => {
+  test('list.getMiddle() should return middle node', () => {
     let list = LinkedList.fromArray([4, 5, 6]);
     let middle = list.getMiddle();
     expect(middle.value).toEqual(5);
@@ -251,7 +251,7 @@ describe('Stretch Goal: Common Interview Questions', () => {
     expect(middle.value).toEqual(5);
   });
 
-  test.skip('list.reverse() should reverse the list', () => {
+  test('list.reverse() should reverse the list', () => {
     let list = LinkedList.fromArray([4, 5, 6]);
     list.reverse();
     expect(list.root.value).toEqual(6);
@@ -261,26 +261,26 @@ describe('Stretch Goal: Common Interview Questions', () => {
 });
 
 describe('Stretch Goal: Functional Methods', () => {
-  test.skip('list.forEach(appender) should append items to a string', () => {
+  test('list.forEach(appender) should append items to a string', () => {
     let result = '';
     let list = LinkedList.fromArray([1,2,3]);
     list.forEach(n => result += n.value + ' ');
     expect(result).toEqual('1 2 3 ');
   });
 
-  test.skip('list.map(doubler) should double a list', () => {
+  test('list.map(doubler) should double a list', () => {
     let list = LinkedList.fromArray([1,2,3]);
     let result = list.map(n => n.value * 2);
     expect(result).toEqual(LinkedList.fromArray([2,4,6]));
   });
 
-  test.skip('list.filter(isEven) should filter out odd numbers', () => {
+  test('list.filter(isEven) should filter out odd numbers', () => {
     let list = LinkedList.fromArray([1,2,3,4]);
     let result = list.filter(n => n.value % 2 === 0);
     expect(result).toEqual(LinkedList.fromArray([2,4]));
   })
 
-  test.skip('list.reduce(adder) should reduce list', () => {
+  test('list.reduce(adder) should reduce list', () => {
     let list = LinkedList.fromArray([1,2,3]);
     let result = list.reduce((p, n) => p + n.value, 0);
     expect(result).toEqual(6);

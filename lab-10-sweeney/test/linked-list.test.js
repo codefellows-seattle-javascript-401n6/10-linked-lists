3e1990e09e6349c9ada3ad5f903a32c931d046a7
Caitlin <caitlinsweeneylmt@gmail.com>
2018-06-29 14:12:46 -0700

    did a bit more

--- lab-10-sweeney/test/linked-list.test.js
+++ lab-10-sweeney/test/linked-list.test.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const {LinkedList, ListNode} = require('../lib/linked-list');
 
@@ -7,11 +7,10 @@ describe('Essential creation tests', function(){
     expect(1).toEqual(1);
   });
 
-  test('new ListNode should create a node', () => {
+  test.skip('new ListNode should create a node', () => {
     let blue = new ListNode(4);
     expect(blue.value).toEqual(4);
     expect(blue.next).toEqual(null);
-  
 
     let green = new ListNode(3, blue);
     expect(green.value).toEqual(3);
@@ -19,7 +18,7 @@ describe('Essential creation tests', function(){
     expect(green.next.value).toEqual(4);
   });
 
-  test('can create a LinkedList and attach nodes together', () => {
+  test.skip('can create a LinkedList and attach nodes together', () => {
     let list = new LinkedList();
     let three = new ListNode(3);
     let four = new ListNode(4);
@@ -32,11 +31,10 @@ describe('Essential creation tests', function(){
     expect(list.root.next.next).toEqual(null);
   });
 
-  test('list.toString() represents the list', () => {
+  test.skip('list.toString() represents the list', () => {
     let list = new LinkedList();
     let three = new ListNode(3);
     let four = new ListNode(4);
-
 
     list.root = three;
     list.root.next = four;
@@ -44,13 +42,12 @@ describe('Essential creation tests', function(){
     expect(list.toString()).toEqual('root -> 3 -> 4 -> null');
   });
 
-  test('list.fromArray builds a linked list from an Array', () => {
+  test.skip('list.fromArray builds a linked list from an Array', () => {
     let list = LinkedList.fromArray([1,2,3,4])
     expect(list.root.value).toEqual(1)
     expect(list.root.next.value).toEqual(2)
     expect(list.root.next.next.value).toEqual(3)
     expect(list.root.next.next.next.value).toEqual(4)
-    console.log(list.root.value);
   });
 });
 
